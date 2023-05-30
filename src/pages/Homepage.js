d27447471ca70b500b2ed701e234afd64644796d
Divya Sharma <divyaonline20@gmail.com>
2023-05-30 22:26:25 +0530

    homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,81 @@
-import React from 'react'
-import './Homepage.css';
+import React from "react";
+import "./Homepage.css";
+import vecto from "../images/Screenshot 2023-05-30 215145.png";
 const Homepage = () => {
   return (
-    <div className='homepage'>Homepage</div>
-  )
-}
+    <div className="homepage">
+      <div className="homepage--first--section">
+        <div className="homepage--first--heading">
+          Live Chat software that let's you chat from any where!
+          <p className="homepage--first--para">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries,
+          </p>
+          <button className="homepage--first--button">Get Started</button>
+        </div>
+        <img
+          src="https://picsum.photos/400/300"
+          alt="random"
+          className="homepage--first--image"
+        />
+      </div>
+      <div className="homepage--second--section">
+        <img
+          src="https://picsum.photos/400/300"
+          alt="random"
+          className="homepage--second--image"
+        />
+        <div className="homepage--second--heading">
+          Easy to use
+          <p className="homepage--second--para">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries,
+          </p>
+          <button className="homepage--second--button">Get Started</button>
+        </div>
+      </div>
+      <div className="end--page">
+        <div className="end--page--section">
+          <img src={vecto} alt="vecto" className="end--page--image" />
+        </div>
+        <div className="end--page--section2">
+          <div className="end--page--element">
+            <div className="end--page--heading"> Step 1 </div>
+            <p className="end--page--cont">
+              {" "}
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever sinc
+            </p>
+          </div>
+          <div className="end--page--element">
+            <div className="end--page--heading"> Step 2 </div>
+            <p className="end--page--cont">
+              {" "}
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever sinc
+            </p>
+          </div>
+          <div className="end--page--element">
+            <div className="end--page--heading"> Step 3 </div>
+            <p className="end--page--cont">
+              {" "}
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever sinc
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
