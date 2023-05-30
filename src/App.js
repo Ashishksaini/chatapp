986bb6c67510276ede7bae27719cf2d53a49b2a3
Divya Sharma <divyaonline20@gmail.com>
2023-05-30 19:28:16 +0530

    routes are defined

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Todo from "./pages/Todo";
+import Joinpage from "./pages/JoinPage";
+import Homepage from "./pages/Homepage";
 const App = () => {
   return (
-    <div className='app'>App</div>
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="joining--page" element={<Joinpage />}></Route>
+          <Route path="todo" element={<Todo />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
-}
+};
 
-export default App
+export default App;
