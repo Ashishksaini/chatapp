30a148f76a44aa79a9d73a52c2df1384bc21ca2c
Ashish Kumar Saini <ashishksaini2001@gmail.com>
2023-05-31 13:40:52 +0530

    adding todo and card

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,8 +1,31 @@
 import React from 'react'
-
-const Cards = () => {
+import "./Cards.css";
+const Cards = ({date,time,name}) => {
+    let meetingtype;
     return (
-        <div className="cards">Cards</div>
+      <div className='card'>
+        <div className='cards'>
+          <div className='card_avatar'>
+            <img
+              src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjqwm3d3WRtydwtUx550VaHMMx9bQGCiTIHACRPtezkXnFxPCSkrXG0Rx2EC3jiyjAV1I1HdrcTyM&usqp=CAU&ec=48665701'
+              alt='Avatar.jpg'
+            />
+          </div>
+          <div className='card_detail'>
+            <div className='card_avatar_name'>{name}</div>
+            <div className='card_meeting_detail'>
+              <img
+                src='https://i.pinimg.com/originals/0b/d5/c7/0bd5c79a8b47e78e25bd1853d0ee5b60.png'
+                alt=''
+              />
+              <div className='day card_meeting_details'>{date}</div>
+              <div className='time card_meeting_details'>{time}</div>
+            </div>
+                </div>
+                <button className='card_start_buttton'>Start</button>
+        </div>
+        <div className='card_cover'></div>
+      </div>
     )
 };
 
--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,9 +1,91 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import Cards from '../components/Cards';
+import './Todo.css'
+import Footer from '../components/Footer';
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import SearchIcon from '@mui/icons-material/Search'
+import { TabScrollButton } from '@mui/material'
 const Todo = () => {
-  return (
-    <div className='todo'>Todo</div>
-  )
+    const data =[];
+    const [val, setval] = useState(data);
+    const [valid, setvalid] = useState(false);
+    useEffect(() => {
+        document.querySelector('.name').value = "";
+        document.querySelector('.date_time').value = "";
+    }, [val,valid]);
+    let addCard = (e) => {
+
+        let cardadder = document.querySelector('.cardAdder');
+        cardadder.classList.remove("hidden");
+        let cardx = document.querySelector('.todo_meetings');
+        cardx.classList.add("hidden");
+        document.querySelector('.todo_card_add').classList.add('hidden');
+        
+    }
+    let handleAdd = (e) => {
+        let d=document.querySelector('.name').value;
+        let t = document.querySelector('.date_time').value;
+        if (d.length == 0 || t.length == 0 ) { 
+            alert("Enter valid data");
+            setvalid(!valid);
+            return;
+        };
+        const nCard = {
+            date: t.substr(0, 10),
+            time: t.substr(11),
+            name: d
+        };
+        setval([nCard,...val]);
+        document.querySelector('.cardAdder').classList.add('hidden');
+        document.querySelector('.todo_meetings').classList.remove('hidden');
+        document.querySelector('.todo_card_add').classList.remove('hidden');
+    }
+    return (
+      <div className='todo'>
+        <div className='todo_nav'>
+          <span className='todo_nav_message'>
+            <b>Chat & Meeting</b>
+          </span>
+          <div className='todo_nav_icons'>
+            <button className='todo_nav_icon'>
+              <NotificationsIcon />
+            </button>
+            <button className='todo_nav_icon'>
+              <SearchIcon />
+            </button>
+          </div>
+        </div>
+        <div className='todo_meeting_options'>
+          <button className='todo_meeting_option'>1 on 1</button>
+          <button className='todo_meeting_option'>Team</button>
+        </div>
+        <div className='todo_meetings'>
+          {/* <TabScrollButton/> */}
+                {val.map((e) => {
+                    // console.log(e);
+            return <Cards {...e} ></Cards>
+          })}
+        </div>
+        <div className='todo_card_add'>
+          <button className='todo_card_add_button' onClick={addCard}>+</button>
+            </div>
+            {/* cardAdder */}
+            <div className="cardAdder hidden">
+                <div className="cardAdder_cover"></div>
+                <div className='cardAdder_content'>
+                <div> Name   : <input type="text" autoComplete='on'  className="name"/></div>
+                <div>
+                 Venue    : <input type="datetime-local" className='date_time'/>
+                </div>
+                    <button onClick={handleAdd}>Add</button>
+                </div>
+            </div>
+        <div className='todo_footer'>
+          <Footer />
+        </div>
+      </div>
+    )
 }
 
-export default Todo
+export default Todo;
